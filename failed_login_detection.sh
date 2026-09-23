7978e7b47b14e073e3f631dfcd91a215f3d27536
Weibo0123 <180469664+Weibo0123@users.noreply.github.com>
2026-09-23 10:32:36 -0500

    Added the timestamp

--- failed_login_detection.sh
+++ failed_login_detection.sh
@@ -2,11 +2,16 @@
 
 PATTERN="Failed password"
 ALERT_LOG="./alert.log"
+YEAR=$(date +%Y)
 
 while IFS= read -r line; do
     rhost=$(echo "$line" | grep -oP '(?<=from )\S+(?= port)')
-    user=$(echo "$line" | awk -F'[= ]' '{{print $(NF-5)}}')
-    message+="rhost: $rhost, user: $user"$'\n'
-done < <(journalctl -u ssh --no-pager -o cat | grep "$PATTERN")
+    user=$(echo "$line" | awk -F'[= ]' '{print $(NF-5)}')
+    month=$(echo "$line" | awk -F'[= ]' '{print $1}')
+    month=$(date -d "$month 1" '+%m')
+    data=$(echo "$line" | awk -F'[= ]' '{print $2}')
+    time=$(echo "$line" | awk -F'[= ]' '{print $3}')
+    message+="$YEAR-$month-$data $time rhost: $rhost, user: $user"$'\n'
+done < <(journalctl -u ssh --no-pager | grep "$PATTERN")
 
 echo -e "Invalid User Login Attempts detected:\n$message" | tee -a "$ALERT_LOG"
